44130056265c0ea81c08f79f09b1fb3894753ea4
putusuartana <55476163+putusuartana@users.noreply.github.com>
2019-09-20 22:38:41 +0800

    Update safelink_caraputu.js

--- safelink_caraputu.js
+++ safelink_caraputu.js
@@ -23,7 +23,7 @@ var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
 if(x=="1"){xxx="social-bookmarking-and-why-it-is-so.html"}
 if(x=="2"){xxx="social-bookmarking-and-dedicated.html"}
-if(x=="3"){xxx="seo-adwords-benefits-for-local.htmll"}
+if(x=="3"){xxx="seo-adwords-benefits-for-local.html"}
 if(x=="4"){xxx="common-ppc-mistakes-that-can-get-you.html"}a_to_ve[i].href="https://safelink.caraputu.com/2017/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
